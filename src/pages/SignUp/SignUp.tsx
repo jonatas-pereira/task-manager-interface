63b90013fc7d9cf1a87391a74921d65cb075286e
Jônatas Pereira <77009157+jonatas-pereira@users.noreply.github.com>
2024-06-11 22:08:22 -0300

    Update SignUp.tsx

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,45 @@
+import { IoCloseSharp } from "react-icons/io5"
+import Vetor from "../../assets/VectorSignUp.svg"
+import VectorSignUp from "../../assets/signUpPicture.png"
+
 const SignUp = () => {
     return (
-        <h1>hello, world!!!</h1>
+        <div className="flex justify-center items-center w-full h-screen font-fontCaveat bg-gradient-to-r from-whiteI to-whiteF">
+            <main className="flex justify-center items-center w-full h-5/6 max-w-screen-xl min-w-80 rounded-xl secondary-bg-color sm:flex-row">
+                <div className="relative flex flex-col justify-center items-center h-full w-full rounded-2xl m-10 bg-secondS sm:basis-1/2 sm:rounded-l-2xl sm:rounded-r-none sm:m-0">
+                    <img src={Vetor} className="absolute h-full w-auto left-full hidden sm:block" alt="" />
+                    <h1 className="text-6xl mb-12 text-titleC caveat-primary">Cadastre-se</h1>
+                    <div className="w-full p-4 sm:w-3/5 sm:p-0">
+                        <form className="w-full text-2xl ">
+                            <p className="text-dark">Email</p>
+                            <div className="mb-7">
+                                <input className="w-full text-xl pt-2 bg-transparent text-gray-500 focus:outline-none" type="text" placeholder='Digite seu email'/>
+                                <div className="w-full h-1 bg-gradient-to-r from-btnF to-btnI"></div>
+                            </div>
+                            <p className="text-dark">Senha</p>
+                            <div className="mb-7">
+                                <input className="w-full text-xl pt-2 bg-transparent text-gray-500 focus:outline-none" type="text" placeholder='Digite sua senha'/>
+                                <div className="w-full h-1 bg-gradient-to-r from-btnF to-btnI"></div>
+                            </div>
+                            <p className="text-dark">Confirme a senha</p>
+                            <div className="mb-7">
+                                <input className="w-full text-xl pt-2 bg-transparent text-gray-500 focus:outline-none" type="text" placeholder='Digite sua senha novamente'/>
+                                <div className="w-full h-1 bg-gradient-to-r from-btnF to-btnI"></div>
+                            </div>
+                        </form>
+                        <div className='w-full flex justify-around mt-10'>
+                        <button className="w-5/12 h-10 rounded-lg border-none text-2xl bg-gradient-to-r from-btnI to-btnF text-zinc-50">Sign in</button>
+                        <button className="w-5/12 h-10 rounded-lg border-solid border-2 text-2xl text-btnColor border-btnColor">Signup</button>
+                     </div>
+                    </div>
+
+                </div>
+                <div className="relative basis-1/2 hidden justify-center items-center h-full rounded-2xl bg-gradient-to-r from-blueI to-blueF sm:flex sm:rounded-l-none">
+                    <img src={VectorSignUp} className="w-72" alt="" />
+                    <IoCloseSharp className="absolute right-6 top-6 text-zinc-50 size-8 "/>
+                </div>
+            </main>
+        </div>
     )
 }
 
