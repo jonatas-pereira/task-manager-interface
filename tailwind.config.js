ba4f073c1c359ed986211aad6e7470f2be1c417d
Jônatas Pereira <77009157+jonatas-pereira@users.noreply.github.com>
2024-06-11 21:58:10 -0300

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'whiteI': '#91B3FA',
+        'whiteF': '#D7E2F9',
+        'blueI': '#4E51FE',
+        'blueF': '#7D97F4',
+        'inputI': '#91B3FA',
+        'inputF': '#5A5CEB',
+        'btnI': '#91B3FA',
+        'btnF': '#797AE0',
+        'btnColor': '#6E70D9',
+        'titleC': '#4F52FF',
+        'secondS': '#E9F0FF',
+        'light': '#8E8E8E',
+        'dark': '#5E5E5E'
+      },
+      fontFamily: {
+        'fontCaveat': '"Caveat", cursive'
+      },
+    },
   },
   plugins: [],
 }
