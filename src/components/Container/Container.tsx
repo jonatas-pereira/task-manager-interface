59c883418e052426f714c2ca7caf0577e5edfd7c
Jônatas Pereira <77009157+jonatas-pereira@users.noreply.github.com>
2024-06-14 22:04:21 -0300

    Update Container.tsx

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,11 +1,11 @@
 type Props = {
-    children: JSX.Element | JSX.Element[] | string | string[]
+  children: JSX.Element | JSX.Element[] | string | string[]
 }
 
 const Container = ({ children }: Props) => {
   return (
     <div className={"flex justify-center items-center w-full h-screen font-roboto-regular font-normal bg-gradient-to-r from-whiteF to-whiteI"}>
-      <main className="flex justify-center items-center w-full h-5/6 max-w-screen-xl min-w-80 rounded-xl sm:flex-row">
+      <main className="flex justify-center items-center w-full h-5/6 max-w-screen-xl min-w-80 rounded-xl sm:flex-row sm:bg-secondS">
         { children }
       </main>
     </div>
