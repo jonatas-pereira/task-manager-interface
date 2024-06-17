a11c7f07fcaa5193e62b1964b1ad02aef77a4da8
Jônatas Pereira <77009157+jonatas-pereira@users.noreply.github.com>
2024-06-17 20:36:39 -0300

    Update LandingPage.tsx

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,25 +1,19 @@
-import imgInicial from "../../assets/imgInicial.png"
-import imgInicial2 from "../../assets/imgInicial2.png"
 import Container from "../../components/Container/Container.tsx"
-import { FaHeart } from "react-icons/fa"
+import ImgLP from "../../assets/landinpImg.png"
+import Vetor from "../../assets/VectorSignIn.svg"
 
 const LandingPage = () => {
   return (
-    <>
+    <div>
       <Container>
-        <div>
-          <img src={imgInicial} className=" w-72" alt="" />
-          <img src={imgInicial2}  className="w-72" alt="" />
+        <div className="basis-1/2 hidden justify-center items-center h-full rounded-l-xl bg-gradient-to-r from-blueI to-blueF sm:flex">
+          <img src={ImgLP} className=" w-52 lg:w-80" alt="" />
+        </div>
+        <div className="relative w-full h-full basis-1/2 ">
+          <img className=" h-full w-auto absolute right-full hidden sm:block" src={Vetor} alt="" />
         </div>
       </Container>
-      <footer className="flex justify-center items-center">
-        <div className="flex justify-end items-center w-full max-w-7xl pb-5 mr-10 absolute bottom-0">
-          <p>Feito com</p>
-          <FaHeart className=" text-red-600 mx-2" />
-          <p>por Jônatas.</p>
-        </div>
-      </footer>
-    </>
+    </div>
   )
 }
 
